5b9ac749cccfea3609540372c6c7f7a194295c9c
Daniel Eystberg Sørensen Anderson <danieleystberg@daniels-mbp.lan>
2024-05-14 08:39:17 +0200

    Added 'history/memory' to llm, can 'repeat' previous command'

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from langchain.prompts import (
         )
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from langchain.schema import SystemMessage
+from langchain.memory import ConversationBufferMemory
+
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
-
+from tools.report import write_report_tool
 load_dotenv()
 
 
@@ -26,6 +28,8 @@ prompt = ChatPromptTemplate(
                                    "or what columns exist. Instead use the 'describe_tables' function"
                                    )
                           ),
+            # Add here, so it comes before any new message(History before new events)
+            MessagesPlaceholder(variable_name="chat_history"),
             HumanMessagePromptTemplate.from_template("{input}"),
             # kind of similar to memory, (msg, functions) -> + ai message -> full msg to llm
             MessagesPlaceholder(variable_name="agent_scratchpad")
@@ -33,8 +37,9 @@ prompt = ChatPromptTemplate(
         input_variables=["input"]
         )
 
-
-tools = [run_query_tool, describe_tables_tool]
+# 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+tools = [run_query_tool, describe_tables_tool, write_report_tool]
 agent = OpenAIFunctionsAgent(
         llm=chat,
         prompt=prompt,
@@ -44,10 +49,20 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
         agent=agent,
         verbose=True,
-        tools=tools
+        tools=tools,
+        memory=memory
         )
 
 
 # agent_executor("How many users are in the database?")
-agent_executor("How many users have provided a shipping address?")
+# agent_executor("How many users have provided a shipping address?")
+# agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
+agent_executor(
+        "How many orders are there? Write the result to an html report."
+               )
+
+# Only works if there is some "memory" to llm
+agent_executor(
+        "Repeat the exact same process for users."
+               )
 
